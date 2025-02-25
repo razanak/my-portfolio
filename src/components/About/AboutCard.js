b055914570f0a8b3a2d0f924133ff5049f402c8d
razanak <razanabukhurj@gmail.com>
2025-02-25 19:24:45 +0200

    Updated project with new changes

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Razan Abu Khurj </span>
-            from <span className="purple"> Palestine.</span>
+            from <span className="purple"> Palestine. </span>
           
             I have completed my Bachelor of Science (B.S.) in Computer Engineering from Eastern Mediterranean University
             <br />
