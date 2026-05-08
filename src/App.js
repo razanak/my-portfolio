28df2112806a4837ab74a0469e750c7c265915f2
razanak <razanabukhurj@gmail.com>
2026-05-08 14:36:33 +0100

    fix refresh 404 using HashRouter

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
+// To this:
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate
